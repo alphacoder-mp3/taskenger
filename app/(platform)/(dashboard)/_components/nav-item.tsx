2b64d1db31aea46eb4a63735330b616f227fa249
alphacoder-mp3 <shindeakshay223@gmail.com>
2023-12-11 20:35:52 +0530

    New development, mobile side bar, navbar, navitem, sidebar components have been developed. And also did zustand setup

--- app/(platform)/(dashboard)/_components/nav-item.tsx
+++ app/(platform)/(dashboard)/_components/nav-item.tsx
@@ -12,6 +12,7 @@ import {
 import { cn } from "@/lib/utils";
 import { AccordionItem, AccordionTrigger, AccordionContent } from "@/components/ui/accordion";
 import { Button } from "@/components/ui/button"
+import { Skeleton } from "@/components/ui/skeleton";
 
 export type Organization = {
     id: string;
@@ -110,5 +111,16 @@ export const NavItem = ({
                 }
             </AccordionContent>
         </AccordionItem>
+    )
+}
+
+NavItem.Skeleton = function SkeletonNavItem() {
+    return (
+        <div className="flex items-center gap-x-2">
+            <div className="w-10 h-10 relative shrink-0">
+                <Skeleton className="h-full w-full absolute" />
+            </div>
+            <Skeleton className="h-10 w-full" />
+        </div>
     )
 }
