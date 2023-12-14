16d26967affa009b1f07aa44b8a1627917a35fed
alphacoder-mp3 <shindeakshay223@gmail.com>
2023-12-14 20:03:28 +0530

    Pushing updates, zod implementation done, server actions started

---
+++ actions/create-board.ts
@@ -0,0 +1,24 @@
+'use server';
+
+import { z } from 'zod';
+
+import { db } from '@/lib/db';
+import { revalidatePath } from 'next/cache';
+
+const CreateBoard = z.object({
+  title: z.string(),
+});
+
+export async function create(formData: FormData) {
+  const { title } = CreateBoard.parse({
+    title: formData.get('title'),
+  });
+
+  await db.board.create({
+    data: {
+      title,
+    },
+  });
+
+  revalidatePath('/organization/org_2ZJE4RBNMgCWsUOPNzQrI3jhTWQ');
+}
--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,9 +1,29 @@
-const OrganizationIdPage = () => {
-    return (
-        <div>
-            Organization Page
-        </div>
-    )
-}
+import { create } from '@/actions/create-board';
+import { db } from '@/lib/db';
+
+import { Button } from '@/components/ui/button';
+
+const OrganizationIdPage = async () => {
+  const boards = await db.board.findMany();
+  return (
+    <div className="flex flex-col space-y-4">
+      <form action={create}>
+        <input
+          id="title"
+          name="title"
+          required
+          placeholder="Enter a board title"
+          className="border-black border p-1"
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+      <div className="space-y-2">
+        {boards.map(board => (
+          <div key={board.id}>{board.title}</div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default OrganizationIdPage;
