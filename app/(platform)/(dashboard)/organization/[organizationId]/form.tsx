8a5689e24a13eaa2dad8886028d0d69e39a85b00
alphacoder-mp3 <shindeakshay223@gmail.com>
2023-12-16 15:21:58 +0530

    Implementation of prisma, zod and server actions

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
@@ -0,0 +1,18 @@
+'use client';
+import { create, State } from '@/actions/create-board';
+import { useFormState } from 'react-dom';
+import { FormInput } from './form-input';
+import { FormButton } from './form-button';
+
+export const Form = () => {
+  const initialState: State = { message: null, errors: {} };
+  const [state, dispatch] = useFormState(create, initialState);
+  return (
+    <form action={dispatch}>
+      <div className="flex flex-col space-y-2">
+        <FormInput errors={state?.errors} />
+      </div>
+      <FormButton />
+    </form>
+  );
+};
--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,25 +1,16 @@
-import { create } from '@/actions/create-board';
 import { db } from '@/lib/db';
 
-import { Button } from '@/components/ui/button';
+import { Board } from './board';
+import { Form } from './form';
 
 const OrganizationIdPage = async () => {
   const boards = await db.board.findMany();
   return (
     <div className="flex flex-col space-y-4">
-      <form action={create}>
-        <input
-          id="title"
-          name="title"
-          required
-          placeholder="Enter a board title"
-          className="border-black border p-1"
-        />
-        <Button type="submit">Submit</Button>
-      </form>
+      <Form />
       <div className="space-y-2">
         {boards.map(board => (
-          <div key={board.id}>{board.title}</div>
+          <Board key={board.id} title={board.title} id={board.id} />
         ))}
       </div>
     </div>
