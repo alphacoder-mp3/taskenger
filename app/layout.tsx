0c43fb7083fe70ad348975609d9e959284461ef0
alphacoder-mp3 <shindeakshay223@gmail.com>
2023-12-23 19:34:08 +0530

    Adding web analytics and making SEO improvements

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,80 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import { Analytics } from '@vercel/analytics/react';
 
-import './globals.css'
-import { siteCofig } from '@/config/site'
+import './globals.css';
+import { siteConfig } from '@/config/site';
 
-
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: {
-    default: siteCofig.name,
-    template: `%s | ${siteCofig.name}`,
+    default: siteConfig.name,
+    template: `%s | ${siteConfig.name}`,
   },
-  description: siteCofig.description,
+  metadataBase: new URL(siteConfig.url),
+  description: siteConfig.description,
+  keywords: [
+    'Taskenger',
+    'Trello',
+    'Trello clone',
+    'Next.js',
+    'React',
+    'Tailwind CSS',
+    'Server Components',
+    'Sever Actions',
+    'Radix UI',
+  ],
+  authors: [
+    {
+      name: 'Akshay S',
+      url: 'https://akshayshinde.com',
+    },
+  ],
+  creator: 'Akshay',
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: siteConfig.url,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+    images: [
+      {
+        url: siteConfig.ogImage,
+        width: 1200,
+        height: 630,
+        alt: siteConfig.name,
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: siteConfig.name,
+    description: siteConfig.description,
+    images: [siteConfig.ogImage],
+    creator: '@folklore69_',
+  },
   icons: [
     {
-      url: "/logo.svg",
-      href: "/logo.svg",
-    }
-  ]
-}
+      url: '/logo.svg',
+      href: '/logo.svg',
+    },
+  ],
+  manifest: `${siteConfig.url}/site.webmanifest`,
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-      <html lang="en">
-        <body className={inter.className}>{children}</body>
-      </html>
-  )
+    <html lang="en">
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
+    </html>
+  );
 }
--- config/site.ts
+++ config/site.ts
@@ -1,4 +1,6 @@
-export const siteCofig = {
-    name: "Taskenger",
-    description: "Collaborate, manage projects, and reach new productivity peaks"
-}
+export const siteConfig = {
+  name: 'Taskenger',
+  description: 'Collaborate, manage projects, and reach new productivity peaks',
+  url: 'https://taskenger.vercel.app/',
+  ogImage: 'https://ui.shadcn.com/og.jpg',
+};
